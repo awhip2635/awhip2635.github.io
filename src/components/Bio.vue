840f5ad4700175231049f663920d130f7cf84cdf
Alex Whipple <alexwhipple19@gmail.com>
2018-01-05 23:23:49 -0700

    re working everything

---
+++ src/components/Bio.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="container-fluid">
+        <div class="row row-eq-height">
+            <h1>About Me</h1>
+            <div class="col-xs-12 col-sm-12 col-md-2">
+
+                <img class="profileshot" src="Uhh professional.jpg" alt="">
+
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <p class="h3font">Hi, I'm Alex! I am a full-stack web developer, who is eager to learn and expand my skillset. I strive to
+                    create innovative, and well-designed projects that are both visually and functionally appealing to the
+                    client and user alike. In my freetime, I enjoy playing music.
+                </p>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+
+            <div id="langs">
+                <h4>SKILLS</h4>
+                <img src="https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png" alt="html5" height="55px" width="55px">
+                <img src="http://www.blackbirdsolutions.com.au/sites/default/files/styles/medium/public/blog/css3-logo.png?itok=r8kt8WZA"
+                    alt="css" height="55px" width="55px">
+                <img src="https://v4-alpha.getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" height="55px" width="55px">
+
+                <!--<img src="https://www.w3schools.com/angular/pic_angular.jpg" alt="" height="55px" width="55px">-->
+                <img src="https://vuejs.org/images/logo.png" alt="" height="55px" width="55px">
+                <img src="https://www.echosteg.com/images/blog/standard/nodejs_logo.png" alt="" height="55px" width="55px">
+                <img src="https://cdn.codementor.io/assets/tutors/c-sharp-tutors-online.png" alt="" height="55px" width="55px">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/240px-Unofficial_JavaScript_logo_2.svg.png"
+                    alt="" height="55px" width="55px;">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Bio',
+        data() {
+            return {
+
+            }
+        }
+    }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1,
+    h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="container-fluid">
+  <div>
+    <h1>Alex Whipple</h1>
+    <h2>Full Stack Web Developer</h2>
+  </div>
+  <div>
+      <router-link :to="{name: 'Bio'}">
+    <h2>Bio</h2>
+    </router-link>
+    <router-link :to="{name: 'Portfolio'}">
+    <h2>Portfolio</h2>
+    </router-link>
+    <router-link :to="{name: 'Contact'}">
+    <h2>Contact</h2>
+    </router-link>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
